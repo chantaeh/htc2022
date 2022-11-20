20d4d043c21966f6237e5434495abde4a5f74ef3
chantaeh <96901376+chantaeh@users.noreply.github.com>
2022-11-20 10:55:35 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,118 @@
+// credits to Web Dev Simplified for the follow along tutorial
+
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
+  {
+    question: 'What is the square root of 4?',
+    answers: [
+        { text: '2', correct: true },
+        { text: '3', correct: false },
+        { text: '0', correct: false },
+        { text: '10', correct: false }
+    ]
+  },
+  {
+    question: 'What is 3+5?',
+    answers: [
+        { text: '7', correct: false },
+        { text: '8', correct: true },
+        { text: '1', correct: false },
+        { text: '2', correct: false }
+    ]
+  },
+  {
+    question: 'What is 4-8?',
+    answers: [
+        { text: '0', correct: false },
+        { text: '4', correct: false },
+        { text: '-4', correct: true },
+        { text: '2', correct: false }
+    ]
+  },
+  {
+    question: 'What is 6 / 2 ?',
+    answers: [
+        { text: '2', correct: false },
+        { text: '6', correct: false },
+        { text: '8', correct: false },
+        { text: '3', correct: true }
+    ]
+  }
+]
